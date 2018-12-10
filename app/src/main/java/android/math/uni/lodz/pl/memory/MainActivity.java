eb6a506386c24e3100244757af80a4e73b3147e3
Piotr Krzyminski <piotr.krzyminski96@gmail.com>
2018-12-10 15:06:13 +0100

    Initial commit

---
+++ app/src/main/java/android/math/uni/lodz/pl/memory/MainActivity.java
@@ -0,0 +1,13 @@
+package android.math.uni.lodz.pl.memory;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
